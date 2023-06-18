54c6338049de938c4d614a58019d7e0f1a79f634
curry_yang <ychunping@ichx.net>
2023-06-18 15:41:13 +0800

    update docker

--- start.sh
+++ start.sh
@@ -7,8 +7,8 @@ chmod -R 777 storage
 
 composer config -g repo.packagist composer https://mirrors.aliyun.com/composer/
 
-composer clearcache
-composer selfupdate
+#composer clearcache
+#composer selfupdate
 
 composer install
 cp /app.env.example /app.env
